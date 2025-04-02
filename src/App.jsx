e8db28fe67deba07c68765b03f52f11a37bce4e4
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-04-02 14:53:34 +0530

    restarted the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
-import React, { useEffect } from "react";
-// import "./App.css";
-import { useDispatch } from "react-redux";
-import AuthService from "./appwrite/auth";
-import { Header, Footer } from "./components/index";
-import { Outlet } from "react-router-dom";
-import { login, logout } from "./store/authSlice";
+import React from 'react'
 
 export default function App() {
-  const [loading, setLoading] = React.useState(true);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    AuthService.getCurrentUser()
-      .then((user) => {
-        if (user) {
-          dispatch(login({ user }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => setLoading(false));
-  }, []);
   return (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
-      <div className="w-full block">
-        <Header />
-        <main>
-          {/* <Outlet></Outlet> */}
-          dd
-        </main>
-        <Footer />
-      </div>
+    <div className='h-10  bg-red-500 text-white    '>
+      hello
+
     </div>
-  );
+  )
 }
--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Logo({width='100px'}) {
-  return (
-    <div>
-      logo
-    </div>
-  )
-}
